a9243e186f82d23f1ee0b5f0d3e41e3c74c87bda
LalaDuran <adraduran@gmail.com>
2023-09-07 12:01:17 -0300

    Initial commit

---
+++ src/Entidades/Alumno.java
@@ -0,0 +1,90 @@
+
+package Entidades;
+
+import java.time.LocalDate;
+
+public class Alumno {
+    
+    private int idAlumno;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNacim;
+    private boolean activo;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fechaNacim, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacim = fechaNacim;
+        this.activo = activo;
+    }
+
+    public Alumno(int dni, String apellido, String nombre, LocalDate fechaNacim, boolean activo) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacim = fechaNacim;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacim() {
+        return fechaNacim;
+    }
+
+    public void setFechaNacim(LocalDate fechaNacim) {
+        this.fechaNacim = fechaNacim;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNacim=" + fechaNacim + ", activo=" + activo + '}';
+    }
+    
+    
+    
+}
